9776feb95d71048776f72da7ff017c1bfdd7b3b0
Hadi hosaini <hadihosaini313@gmail.com>
2024-09-16 08:43:29 +0330

    four commit

--- components/menu.jsx
+++ components/menu.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Menu = () => {
   return (
-    <div className=''>
+    <div className='flex flex-row gap-2'>
       <Link href={'/'} className='font-serif '>Main</Link>
       <Link href={'/contact'} className='font-serif '>Contact Us</Link>
     </div>
